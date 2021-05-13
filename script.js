e21127acd43e104be904992235f9ebbeb40b8243
Deadida322 <61591520+Deadida322@users.noreply.github.com>
2021-05-13 19:24:20 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function analyse(){
     let source = document.querySelector('#arrays').value
     let to_work = source.split(/ {0,}\, {0,}/).filter(element => element != '')
-    for(let i = 1; i < to_work.length; i++){
-        if (parseInt(to_work[i+1])+parseInt(to_work[i-1]) == 10){
-            let left = to_work[i-1]
+    for(let i = 0; i < to_work.length; i++){
+        if (parseInt(to_work[i])+parseInt(to_work[i+1]) == 10){
+            let left = to_work[i]
             let right = to_work[i+1]
-            to_work[i-1] = `<span class='blue'> ${left} </span>`
+            to_work[i] = `<span class='blue'> ${left} </span>`
             to_work[i+1] = `<span class='red'> ${right} </span>`
         }
     }
