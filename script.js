c60f75b8d686069aa7057f883abdd6bc04f4ccb2
Deadida322 <61591520+Deadida322@users.noreply.github.com>
2021-05-13 19:14:25 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function analyse(){
+    let source = document.querySelector('#arrays').value
+    let to_work = source.split(/ {0,}\, {0,}/).filter(element => element != '')
+    for(let i = 1; i < to_work.length; i++){
+        if (parseInt(to_work[i+1])+parseInt(to_work[i-1]) == 10){
+            let left = to_work[i-1]
+            let right = to_work[i+1]
+            to_work[i-1] = `<span class='blue'> ${left} </span>`
+            to_work[i+1] = `<span class='red'> ${right} </span>`
+        }
+    }
+    to_work = to_work.join(' ');
+    document.getElementById("result").innerHTML = to_work
+}
